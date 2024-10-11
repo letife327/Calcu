72c0f743a6376fcf607bcc73d2070acf70c79f3f
letife327 <mletife327@gmail.com>
2024-10-11 12:53:31 +0400

    was written calculator

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       while(true){
-           calculate();
-       }
+        while (true) {
+            calculate();
+        }
     }
 
     private static void calculate() {
@@ -18,20 +18,24 @@ public class Main {
         String operation = scanner.next();
         switch (operation) {
             case "+":
-                System.out.println(a+b);;
-            break;
+                System.out.println(a + b);
+                ;
+                break;
             case "-":
-                System.out.println(a-b);;
-            break;
+                System.out.println(a - b);
+                ;
+                break;
             case "*":
-                System.out.println(a*b);;
-            break;
+                System.out.println(a * b);
+                ;
+                break;
             case "/":
-                 if(b==0){System.err.println("Cannot be divided by zero"); }
-                 else{
-                     System.out.println(a / b);
-                 }
-            break;
+                if (b == 0) {
+                    System.err.println("Cannot be divided by zero");
+                } else {
+                    System.out.println(a / b);
+                }
+                break;
             default:
                 System.out.println("Invalid operation");
         }
